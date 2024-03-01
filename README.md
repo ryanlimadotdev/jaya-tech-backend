b14df62ef229c54b8bf626b2acd99d9ef97b3b94
ryanlimadotdev <me@ryanlima.dev>
2024-03-01 11:57:10 -0300

    fix: fix type of example values in Swagger

--- README.md
+++ README.md
@@ -7,17 +7,17 @@ Tecnologias usadas:
 - MySQL
 
 ### Getting started
-_Confira os volumes_ 
+_Confira os volumes_
 
 #### Inicie o servidor (porta padrão do framework: 9501):
 ```bash
 cp .env.example .env
 docker-compose build
-docker-compose up
+docker-compose up -d
 docker exec -it hyperf bash
 composer install -o
 php bin/hyperf.php migrate
-php bin/hyperf.php start
+php bin/hyperf.php start -d
 ```
 
 #### Realize a suíte de testes:
@@ -34,5 +34,9 @@ composer test
 - Disponibilização estática do Swagger
 - Attributes para documentar os controllers e gerar automaticamente a documentação da API, apesar de "poluir os controllers", em se tratando que uma documentação e de que os Attributes de hyperf/swagger são compatíveis com os padrões do Swagger framework-agnostic
 
-#### Dica para PHPStorm
+#### Dicas
 - Caso utilize o PHPStorm, recomendo marcar o diretório _runtime_ como excluded, pois os arquivos de proxy tem o mesmo nome de classe que as originais.
+- Como é uma aplicação de PHP Cli e não FPM, o debbuging será diferente do convencional. Recomendo a utilização da biblioteca spatie/ray e alguma image do [Buggregator](https://github.com/buggregator/laravel-app) para tal.
+
+### Next steps
+- Após a criação da pasta vendor, o entrypoint do container do hyperf pode ser substituído para "php bin/hyperf.php start" ou "php bin/hyperf.php server:watch" para o modo com hot reload.
--- app/Schema/PaymentCreationRequestSchema.php
+++ app/Schema/PaymentCreationRequestSchema.php
@@ -10,15 +10,15 @@ use OpenApi\Attributes\Schema;
 #[Schema(
 	required: ['transaction_amount', 'installments', 'token', 'payment_method_id', 'payer'],
 	properties: [
-		new OA\Property(property: 'transaction_amount', description: 'Transaction value', type: 'float', example: '259.9'),
-		new OA\Property(property: 'installments', description: 'Number of installments', type: 'integer', minimum: 1),
+		new OA\Property(property: 'transaction_amount', description: 'Transaction value', type: 'float', example: 259.9),
+		new OA\Property(property: 'installments', description: 'Number of installments', type: 'integer', minimum: 1, example: 1),
 		new OA\Property(property: 'token', description: 'User provided identification token', type: 'string'),
 		new OA\Property(property: 'payment_method_id', description: 'The identification of the payment method', type: 'string'),
 		new OA\Property(property: 'payer', description: 'Payer information', required: ['email', 'identification'], properties: [
-			new OA\Property(property: 'email', type: 'string', default: 'example@email.com'),
+			new OA\Property(property: 'email', type: 'string', example: 'example@email.com'),
 			new OA\Property(property: 'identification', required: ['type', 'number'], properties: [
-				new OA\Property(property: 'type', type: 'string', default: 'CPF', anyOf: ['CPF', 'CNPJ']),
-				new OA\Property(property: 'number', type: 'string', default: '12345678910')
+				new OA\Property(property: 'type', type: 'string', example: 'CPF', anyOf: ['CPF', 'CNPJ']),
+				new OA\Property(property: 'number', type: 'string', example: '12345678910')
 			], type: 'object')
 		], type: 'object'),
 	]
